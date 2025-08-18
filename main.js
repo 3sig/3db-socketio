ae761b9a6fd28116f215e6d42f86acd701679c7f
Aaron Santiago <me@aaron.work>
2025-08-17 22:47:16 -0400

    socketio plugin connecting to server

--- main.js
+++ main.js
@@ -1,51 +1,18 @@
-
+import { io } from "socket.io-client";
 
 export {
-  onBeforeInitialize,
   onInitialize,
-  onAfterInitialize,
-
-  onBeforeUpdateEntry,
   onUpdateEntry,
-  onAfterUpdateEntry,
-
-  onBeforeGetEntry,
-  onGetEntry,
-  onAfterGetEntry,
-}
-
-async function onBeforeInitialize(config, runApi) {
-  console.log('onBeforeInitialize', config["hello"]);
 }
 
 async function onInitialize(config, runApi) {
-  console.log('onInitialize');
+  const socket = io("ws://" + config.serverUrl);
+  socket.on("connect", () => {
+    console.log("Connected to server");
+  });
 }
 
-async function onAfterInitialize(config, runApi) {
-  console.log("onAfterInitialize");
-}
-
-async function onBeforeUpdateEntry(config, runApi, id, data) {
-  console.log("onBeforeUpdateEntry");
-}
 
 async function onUpdateEntry(config, runApi, id, data) {
   console.log("onUpdateEntry");
-}
-
-async function onAfterUpdateEntry(config, runApi, id, data) {
-  console.log("onAfterUpdateEntry");
-}
-
-async function onBeforeGetEntry(config, runApi, id) {
-  console.log("onBeforeGetEntry");
-}
-
-async function onGetEntry(config, runApi, id) {
-  console.log("onGetEntry");
-}
-
-async function onAfterGetEntry(config, runApi, id) {
-  console.log("onAfterGetEntry");
 }
